d83e24c047ba7e99c4b92b54ffc00fb5253d7a80
oakio <ilya.dubadenko@gmail.com>
2014-04-29 23:15:10 +0400

    Added circular buffer implementation

---
+++ Orbit.Tests/RingBufferTests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+
+namespace Codestellation.Orbit.Tests
+{
+    [TestFixture]
+    public class RingBufferTests
+    {
+        private RingBuffer<int> _buffer;
+
+        [SetUp]
+        public void Setup()
+        {
+            _buffer = new RingBuffer<int>(3);
+        }
+
+        [Test]
+        public void Should_set_size_as_power_of_two()
+        {
+            Assert.That(_buffer.Size, Is.EqualTo(8));
+        }
+
+        [Test]
+        [TestCase(0, 0)]
+        [TestCase(5, 5)]
+        [TestCase(5, 13)]
+        [TestCase(11, 19)]
+        [TestCase(25, 41)]
+        public void Should_have_accessor_with_circular_behavior(int setIndex, int getIndex)
+        {
+            _buffer[setIndex] = 42;
+
+            Assert.That(_buffer[getIndex], Is.EqualTo(42));
+        }
+    }
+}
---
+++ Orbit/RingBuffer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Codestellation.Orbit
+{
+    public class RingBuffer<T>
+    {
+        private readonly T[] _buffer;
+
+        private readonly int _size;
+        private readonly int _mask;
+        public int Size
+        {
+            get { return _size; }
+        }
+
+        public RingBuffer(uint power) : this(power, () => default(T))
+        {
+        }
+
+        public RingBuffer(uint power, Func<T> factory)
+        {
+            if (power == 0)
+            {
+                throw new ArgumentException(string.Format("Power should be greater than 0, but was {0}", power), "power");
+            }
+            _size = (1 << (int)power);
+            _mask = _size - 1;
+            
+            _buffer = new T[_size];
+            for (int i = 0; i < _size; i++)
+            {
+                _buffer[i] = factory();
+            }
+        }
+
+        public T this[long sequence]
+        {
+            get
+            {
+                long index = CalcIndex(sequence);
+                return _buffer[index];
+            }
+            set
+            {
+                long index = CalcIndex(sequence);
+                _buffer[index] = value;
+            }
+        }
+
+        private long CalcIndex(long sequence)
+        {
+            return sequence & _mask;
+        }
+    }
+}
