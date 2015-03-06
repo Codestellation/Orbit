866dd75a80b0e1508a1b2f2924f788a9ac875966
Jury Soldatenkov <yurysold@gmail.com>
2015-03-06 11:56:53 +0300

    Installed codestellation synthesis

---
+++ build.ps1
@@ -0,0 +1,27 @@
+﻿param(
+        [string]$company,
+        [string]$copyright
+	)
+
+$ErrorActionPreference = 'Stop'
+
+$invname = $MyInvocation.InvocationName
+$scriptPath = Split-Path $MyInvocation.InvocationName
+
+if($scriptPath)
+{
+    Set-Location $scriptPath    
+}
+
+write "Restore solution-wide packages"
+
+.\.nuget\nuget.exe install .\.nuget\packages.config -o packages
+
+$psakeModule = Get-ChildItem psake.psm1 -Path $scriptPath -Recurse
+
+Import-Module $psakeModule.FullName -force
+
+$default = (Get-ChildItem synthesis.ps1 -Path $scriptPath -Recurse).FullName
+$basedir =  (Get-Item(Get-Location)).FullName
+
+Invoke-psake -framework '4.0' -buildFile $default -parameters @{ basedir=$basedir; company=$company; copyright=$copyright}
