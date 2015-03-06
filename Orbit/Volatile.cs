66e03242dc7764651fe3cd240805f38caba70e90
Jury Soldatenkov <yurysold@gmail.com>
2015-03-06 12:24:17 +0300

    Use compiler symbols to compile both NET40 and NET45

---
+++ Orbit/Volatile.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+using System.Threading;
+
+namespace Codestellation.Orbit
+{
+#if NET40    
+    public static class Volatile
+    {
+        public static void Write(ref long location, long value)
+        {
+            Interlocked.Exchange(ref location, value);
+        }
+
+        public static long Read(ref long location)
+        {
+            return Interlocked.Read(ref location);
+        }
+    }
+#endif
+}
